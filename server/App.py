3869e4e7c57284025d4b23e9d8a12efbc54e1dd1
root <root@ip-172-31-14-201.ap-northeast-2.compute.internal>
2017-09-02 19:33:44 +0000

    Modified

--- server/App.py
+++ server/App.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from Security import authenticate, identity
-from Vault import *
 import argparse
 from resources.samba import SambaList, Samba
 from resources.vpn import VPNList, VPN
@@ -21,8 +20,6 @@ app = Flask(__name__)
 app.secret_key = "M3r1d14n"
 api = Api(app)
 jwt = JWT(app, authenticate, identity) # /auth
-
-
 
 if __name__ == "__main__":
 	api.add_resource(SambaList,'/sambausers/') 
