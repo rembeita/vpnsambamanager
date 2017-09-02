c9c2712e2bff8e004e1cf22a1cbf6a1a4a2da159
root <root@ip-172-31-14-201.ap-northeast-2.compute.internal>
2017-09-02 19:46:28 +0000

    Added

--- server/resources/samba.py
+++ server/resources/samba.py
@@ -24,18 +24,20 @@ class SambaList(Resource):
                 return result_list
 
 class Samba(Resource):
-	#parser = reqparse.RequestParser()
-	#parser.add_argument('username', type=str, required=True, help="This field cannot be blank.")
-	#parser.add_argument('password', type=str, required=True, help="This field cannot be blank.")
+	parser = reqparse.RequestParser()
+	parser.add_argument('username', type=str, required=True, help="This field cannot be blank.")
+	parser.add_argument('password', type=str, required=True, help="This field cannot be blank.")
 	def post(self, name):
-	#	data = Samba.parser.parse_args()
-		output = subprocess.run("/usr/sbin/useradd " + name + " -g smbrestricted", shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+		data = Samba.parser.parse_args()
+		output = subprocess.run("/usr/sbin/useradd " + data['username'] + " -g smbrestricted", shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+		print(data['username'])
+		print(data['password'])
 		print(output.stdout)
-		cmd = subprocess.Popen('/usr/bin/smbpasswd -a ' + name , stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True) 
-		cmd.stdin.write(b'123\n')
+		cmd = subprocess.Popen('/usr/bin/smbpasswd -a ' + data['username'] , stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True) 
+		cmd.stdin.write(b''+ str(data['password']).encode('UTF-8') +b'\n')
 		cmd.stdin.flush()
 		print(cmd.stdout)
-		cmd.stdin.write(b'123\n')
+		cmd.stdin.write(b''+ str(data['password']).encode('UTF-8') +b'\n')
 		cmd.stdin.flush()
 		for line in cmd.stdout.readlines():
 			print(line)
