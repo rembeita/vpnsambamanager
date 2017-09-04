97b8195350f7cfa9593fc122902002fe709b1ff2
root <root@ip-172-31-14-201.ap-northeast-2.compute.internal>
2017-09-04 22:41:00 +0000

    delete uiser

--- webapp/sambamanager/mainmenu.py
+++ webapp/sambamanager/mainmenu.py
@@ -13,25 +13,31 @@ import json
 def mainmenu(request):
 	if request.method == 'POST':
 		formvar = request.POST
+		print(dir(formvar))
 		uservalue = str(formvar['user_frm'])
 		passvalue = str(formvar['password_frm'])
+		context = locals()
+		context['USER'] = uservalue
+		context['PASS'] = passvalue
+		userquery = authenticate(username=uservalue, password=passvalue)
+		if userquery is None:
+			message = "User or Password is incorrect."
+			context['MENSAJE'] = message
+			return render(request, 'sambamanager/invaliduserpass.html', context)
 
-	userquery = authenticate(username=uservalue, password=passvalue)
-
-	context = locals()
-
-	if userquery is None:
-		message = "User or Password is incorrect."
-		context['MENSAJE'] = message
-		return render(request, 'sambamanager/invaliduserpass.html', context)
+		print(formvar.keys())
+		if "deleteuser" in formvar.keys(): # (formvar.has_key('deleteuser')):
+			print("entre")
+			deleteuser = str(formvar['deleteuser'])
+			headers = {'Content-type': 'application/json'}
+			url = 'http://localhost:5000/samba/' + deleteuser
+			response = requests.delete(url, data=json.dumps(''), headers=headers )
+			#users_list = getsambausers('http://localhost:5000/sambausers/')
+			#context["users_list"] = users_list["users_list"]
+			#return render(request, 'sambamanager/mainmenu.html', context)
 
 	users_list = getsambausers('http://localhost:5000/sambausers/')
-	context['USER'] = uservalue
 	context["users_list"] = users_list["users_list"]
-	#print(user_list["users_list"])
-	#user = data['user']
-	for i in users_list["users_list"]:
-		print(i)
 	return render(request, 'sambamanager/mainmenu.html', context)
 
 def getsambausers(url):
